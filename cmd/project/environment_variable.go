3e3bc62233a7ad6efc932b883e5937524f9274de
Shogo Tsutsumi <shogo-tsutsumi@retty.me>
2022-09-24 19:13:24 +0900

    A command `project secret create` added to create or update an environment variable

--- cmd/project/environment_variable.go
+++ cmd/project/environment_variable.go
@@ -1,6 +1,9 @@
 package project
 
 import (
+	"fmt"
+	"strings"
+
 	projectapi "github.com/CircleCI-Public/circleci-cli/api/project"
 	"github.com/CircleCI-Public/circleci-cli/cmd/validator"
 	"github.com/olekukonko/tablewriter"
@@ -23,7 +26,21 @@ func newProjectEnvironmentVariableCommand(ops *projectOpts, preRunE validator.Va
 		Args: cobra.ExactArgs(3),
 	}
 
+	var secValue string
+	createVarCommand := &cobra.Command{
+		Short:   "Create an environment variable of a project. The value is read from stdin.",
+		Use:     "create <vcs-type> <org-name> <project-name> <secret-name>",
+		PreRunE: preRunE,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return createProjectEnvironmentVariable(cmd, ops.client, ops.reader, args[0], args[1], args[2], args[3], secValue)
+		},
+		Args: cobra.ExactArgs(4),
+	}
+
+	createVarCommand.Flags().StringVar(&secValue, "secret-value", "", "The secret value to be created. You can also pass it by stdin without this option.")
+
 	cmd.AddCommand(listVarsCommand)
+	cmd.AddCommand(createVarCommand)
 	return cmd
 }
 
@@ -40,6 +57,48 @@ func listProjectEnvironmentVariables(cmd *cobra.Command, client projectapi.Proje
 	for _, envVar := range envVars {
 		table.Append([]string{envVar.Name, envVar.Value})
 	}
+	table.Render()
+
+	return nil
+}
+
+func createProjectEnvironmentVariable(cmd *cobra.Command, client projectapi.ProjectClient, r Reader, vcsType, orgName, projName, name, value string) error {
+	if value == "" {
+		val, err := r.ReadSecretString("Enter the environment variable value and press enter")
+		if err != nil {
+			return err
+		}
+		if val == "" {
+			return fmt.Errorf("the environment variable value must not be empty")
+		}
+		value = val
+	}
+	value = strings.Trim(value, "\r\n")
+
+	existV, err := client.GetEnvironmentVariable(vcsType, orgName, projName, name)
+	if err != nil {
+		return err
+	}
+	if existV != nil {
+		msg := fmt.Sprintf("Environment variable name=%s value=%s already exists. Do you overwrite it?", existV.Name, existV.Value)
+		if !r.AskConfirm(msg) {
+			fmt.Fprintln(cmd.OutOrStdout(), "Canceled")
+			return nil
+		}
+	}
+
+	v, err := client.CreateEnvironmentVariable(vcsType, orgName, projName, projectapi.ProjectEnvironmentVariable{
+		Name:  name,
+		Value: value,
+	})
+	if err != nil {
+		return err
+	}
+
+	table := tablewriter.NewWriter(cmd.OutOrStdout())
+
+	table.SetHeader([]string{"Environment Variable", "Value"})
+	table.Append([]string{v.Name, v.Value})
 	table.Render()
 
 	return nil
--- cmd/project/project.go
+++ cmd/project/project.go
@@ -3,18 +3,44 @@ package project
 import (
 	projectapi "github.com/CircleCI-Public/circleci-cli/api/project"
 	"github.com/CircleCI-Public/circleci-cli/cmd/validator"
+	"github.com/CircleCI-Public/circleci-cli/prompt"
 
 	"github.com/CircleCI-Public/circleci-cli/settings"
 	"github.com/spf13/cobra"
 )
 
+type Reader interface {
+	ReadSecretString(msg string) (string, error)
+	AskConfirm(msg string) bool
+}
+
 type projectOpts struct {
 	client projectapi.ProjectClient
+	reader Reader
+}
+
+type ProjectOption interface {
+	apply(*projectOpts)
+}
+
+type PromptReader struct{}
+
+func (p PromptReader) ReadSecretString(msg string) (string, error) {
+	return prompt.ReadSecretStringFromUser(msg)
+}
+
+func (p PromptReader) AskConfirm(msg string) bool {
+	return prompt.AskUserToConfirm(msg)
 }
 
 // NewProjectCommand generates a cobra command for managing projects
-func NewProjectCommand(config *settings.Config, preRunE validator.Validator) *cobra.Command {
-	var opts projectOpts
+func NewProjectCommand(config *settings.Config, preRunE validator.Validator, opts ...ProjectOption) *cobra.Command {
+	pos := projectOpts{
+		reader: &PromptReader{},
+	}
+	for _, o := range opts {
+		o.apply(&pos)
+	}
 	command := &cobra.Command{
 		Use:   "project",
 		Short: "Operate on projects",
@@ -23,12 +49,24 @@ func NewProjectCommand(config *settings.Config, preRunE validator.Validator) *co
 			if err != nil {
 				return err
 			}
-			opts.client = client
+			pos.client = client
 			return nil
 		},
 	}
 
-	command.AddCommand(newProjectEnvironmentVariableCommand(&opts, preRunE))
+	command.AddCommand(newProjectEnvironmentVariableCommand(&pos, preRunE))
 
 	return command
+}
+
+type CustomReaderProjectOption struct {
+	r Reader
+}
+
+func (c CustomReaderProjectOption) apply(opts *projectOpts) {
+	opts.reader = c.r
+}
+
+func CustomReader(r Reader) ProjectOption {
+	return CustomReaderProjectOption{r}
 }
